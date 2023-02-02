c65f661e7a7df59fd713869a654c50bbfd77e4e3
Arya Jawarkar <aryajawarkar7@gmail.com>
2023-02-02 06:46:29 +0530

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>About us</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque in modi odit accusantium, ducimus eveniet voluptates exercitationem suscipit a illo omnis officia animi, illum pariatur eius delectus ratione nisi? Temporibus.</p>
+    </section>
+  )
+}
+
+export default About
